e51df25a42997777a82494cedb2961c8a56a087c
keumhwanmoon <hwanth85@gmail.com>
2016-09-07 22:26:45 +0900

    code..

--- src/main/java/com/jason/controller/MemberController.java
+++ src/main/java/com/jason/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.jason.domain.Member;
 import com.jason.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,7 +24,7 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @RequestMapping
+    @GetMapping("/members")
     @ResponseBody
     public List<Member> findMembers() {
         return memberService.findMembers();
--- src/main/java/com/jason/service/MemberService.java
+++ src/main/java/com/jason/service/MemberService.java
@@ -14,7 +14,6 @@ import java.util.List;
 @Service
 public class MemberService {
 
-
     private MemberMapper memberMapper;
 
     @Autowired
